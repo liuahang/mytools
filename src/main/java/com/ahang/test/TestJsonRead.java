a82b1030cca6695ec783ffb6f67462dda2fbfc7b
liuhang <liuhang@yun70.com>
2016-11-18 16:09:44 +0800

    修改bug

--- src/main/java/com/ahang/test/TestJsonRead.java
+++ src/main/java/com/ahang/test/TestJsonRead.java
@@ -34,13 +34,13 @@ public class TestJsonRead {
 
 	public static void main(String args[])  throws Exception {
 		TestJsonRead test = new TestJsonRead();
-		Map map = test.getJsonMap();
+		Map<String,Object> map = test.getJsonMap();
 		System.out.println("map==" + map );
 	}
 	
 	public Map  getJsonMap() throws Exception{
 		String json = "";
-		Map map = null;
+		Map<String,Object> map = null;
 		InputStream stream = this.getClass().getClassLoader().getResourceAsStream("json/policy.json");
         StringWriter writer = new StringWriter();
         IOUtils.copy(stream, writer, String.valueOf(Charset.defaultCharset()));
