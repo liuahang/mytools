81e57984406c86deeade25365492e7bc97cd3118
liuhang <liuhang@yun70.com>
2016-11-21 15:36:08 +0800

    添加jdbc，ftp，bean，zip，http等工具

---
+++ src/main/java/com/ahang/utils/IpUtils.java
@@ -0,0 +1,49 @@
+package com.ahang.utils;
+
+import org.apache.commons.lang3.StringUtils;
+import javax.servlet.http.HttpServletRequest;
+/**
+ *    <dependency>
+      <groupId>javax.servlet</groupId>
+      <artifactId>javax.servlet-api</artifactId>
+      <version>3.1.0</version>
+    </dependency>
+ * @author liuhang
+ *
+ */
+public class IpUtils {
+
+	private IpUtils() {
+	}
+
+	/**
+	 * 获取真实ip地址
+	 * @param request
+	 * @return
+	 */
+	public static String getIpAddress(HttpServletRequest request) {
+		if (request == null) {
+			return "unknown";
+		}
+		String ip = request.getHeader("x-forwarded-for");
+		if (StringUtils.isBlank(ip) || ip.equalsIgnoreCase("unknown")) {
+			ip = request.getHeader("Proxy-Client-IP");
+		}
+		if (StringUtils.isBlank(ip) || ip.equalsIgnoreCase("unknown")) {
+			ip = request.getHeader("X-Forwarded-For");
+		}
+		if (StringUtils.isBlank(ip) || ip.equalsIgnoreCase("unknown")) {
+			ip = request.getHeader("WL-Proxy-Client-IP");
+		}
+		if (StringUtils.isBlank(ip) || ip.equalsIgnoreCase("unknown")) {
+			ip = request.getHeader("X-Real-IP");
+		}
+		if (StringUtils.isBlank(ip) || ip.equalsIgnoreCase("unknown")) {
+			ip = request.getRemoteAddr();
+		}
+		if (StringUtils.equals(ip, "0:0:0:0:0:0:0:1")) {
+			ip = "127.0.0.1";
+		}
+		return ip;
+	}
+}
